fc409cb33b8d8f626d31a97d5d722a2ca2ae602a
Ivanqi <512350225@qq.com>
2021-03-10 21:58:27 +0800

    【开发】增加HttpServer

---
+++ src/HttpServer.h
@@ -0,0 +1,47 @@
+#ifndef EVENT_HTTPSERVER_H
+#define EVENT_HTTPSERVER_H
+
+#include "TcpServer.h"
+
+class HttpRequest;
+class HttpResponse;
+
+class HttpServer
+{
+    public:
+        typedef std::function<void (const HttpRequest&, HttpResponse*)> HttpCallback;
+
+    private:
+        TcpServer server_;
+        HttpCallback httpCallback_;
+    
+    public:
+        HttpServer(EventLoop* loop, const InetAddress& listenAddr, const string& name, TcpServer::Option option = TcpServer::kNoReusePort);
+
+        EventLoop* getLoop() const
+        {
+            return server_.getLoop();
+        }
+
+        // 不是线程安全。调用start()之前注册回调
+        void setHttpCallback(const HttpCallback& cb)
+        {
+            httpCallback_ = cb;
+        }
+
+        void setThreadNum(int numThreads)
+        {
+            server_.setThreadNum(numThreads);
+        }
+
+        void start();
+    
+    private:
+        void onConnection(const TcpConnection& conn);
+
+        void onMessage(const TcpConnectionPtr& conn, Buffer *buf, Timestamp receiveTime);
+
+        void onRequest(const TcpConnectionPtr& , const HttpRequest&);
+};
+
+#endif
